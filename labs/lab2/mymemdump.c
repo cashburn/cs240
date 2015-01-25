6d2de0fe8922d8e076cd787b8a12eb8c16e4502e
Colin Ashburn <cashburn@purdue.edu>
2015-01-25 17:58:54 -0500

    2015-01-25

--- labs/lab2/mymemdump.c
+++ labs/lab2/mymemdump.c
@@ -9,17 +9,18 @@ void mymemdump(FILE * fd, char * p , int len) {
     // p[0] will return the element 0 
     // p[1] will return the element 1 and so on
     
-    //int count = 0;
-    //while ((count * 8) < len) {
-    
-    fprintf(fd, "0x%016lX: , LENGTH= %d", (unsigned long) p, len);
-    for (int i = 0; i < len; i++) {
+    int count = 0;
+    while ((count * 16) < len) {
+    p += count * 16;
+    fprintf(fd, "0x%016lX: ", (unsigned long) p);
+    for (int i = (16 * count); i < (16 * (count + 1)); i++) {
       int c = p[i]&0xFF;
         
       // Print first byte as hexadecimal
       fprintf(fd, "%02X ", c);
     }
-    for (int i = 0; i < len; i++) {
+	fprintf(fd, " ");
+    for (int i = (16 * count); i < (16 * (count + 1)); i++) {
       int c = p[i]&0xFF;
       // Print first byte as character
       if (c>=127)
@@ -27,7 +28,7 @@ void mymemdump(FILE * fd, char * p , int len) {
       else
       fprintf(fd, "%c", (c>=32)?c:'.');
     }
-    //count++;
+    count++;
     fprintf(fd,"\n");
-    //}
+    }
 }
