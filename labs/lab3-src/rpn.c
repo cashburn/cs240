d7a57e09a0a97de1a5914763186f9640d983596c
Colin Ashburn <cashburn@purdue.edu>
2015-02-08 15:39:42 -0500

    2015-02-08

--- labs/lab3-src/rpn.c
+++ labs/lab3-src/rpn.c
@@ -16,9 +16,10 @@ double rpn_eval(char * fileName, double x) {
 		exit(1);
 	}
 	int count = 0;
-	int c;
-	char * i;
+	int c, i;
 	char string[6];
+	char numbers[] = "1234567890";
+	numbers[10] = 3;
 	double add(double arg2, double arg1) {
         return arg1 + arg2;
     }
@@ -34,8 +35,8 @@ double rpn_eval(char * fileName, double x) {
 	while ((c = fgetc(fd)) != EOF) {
 		fseek(fd, -1l, SEEK_CUR);
 		fscanf(fd, "%s", string);
-		i = strstr((char *) string, "123456789");
-		if (i != NULL) {
+		i = strcspn(string, numbers);
+		if ((i + 1)) {
 			stack_push(atof(string));
 			while (((c = fgetc(fd)) != '\n') && (c != ' ')) { }
 			count++;
