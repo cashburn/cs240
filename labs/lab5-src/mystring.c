f23a59fa17d9566e8e4267c4017259b557a96c18
Colin Ashburn <cashburn@purdue.edu>
2015-03-22 18:58:23 -0500

    90 of 100

--- labs/lab5-src/mystring.c
+++ labs/lab5-src/mystring.c
@@ -128,7 +128,7 @@ char * mystrreplace( char * src, char * from, char * to)  {
 		src1 = final1;
 		from1 = from;
 		to1 = to;*/
-		if (mystrstr(final, from) != NULL)
+		if (strstr(final, from) != NULL)
 			return mystrreplace(final, from, to);
 
 	return final;
