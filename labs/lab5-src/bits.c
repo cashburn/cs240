d7552ae7beae62c72e661c4470400874f9cea1b2
Colin Ashburn <cashburn@purdue.edu>
2015-03-24 10:15:09 -0400

    90 of 140

--- labs/lab5-src/bits.c
+++ labs/lab5-src/bits.c
@@ -1,15 +1,5 @@
-
-
-// It prints the bits in bitmap as 0s and 1s.
-void printBits(unsigned int bitmap) {
-	for (int i = 31; i >= 0; i--) {
-		printf("%d", getBitAt(bitmap, i));
-	}
-	printf("\n");
-	for (int i = 31; i >= 0; i--)
-		printf("%d", i%10);
-	printf("\n");
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 
 // Sets the ith bit in *bitmap with the value in bitValue (0 or 1)
@@ -20,7 +10,8 @@ void setBitAt( unsigned int *bitmap, int i, int bitValue ) {
 // It returns the bit value (0 or 1) at bit i
 int getBitAt( unsigned int bitmap, unsigned int i) {
 	unsigned int mask = 1 << i;
-	return ((bitmap & mask) >> i;
+	return ((bitmap & mask) >> i);
+}
 // It returns the number of bits with a value "bitValue".
 // if bitValue is 0, it returns the number of 0s. If bitValue is 1, it returns the number of 1s.
 int countBits( unsigned int bitmap, unsigned int bitValue) {
@@ -32,6 +23,17 @@ int countBits( unsigned int bitmap, unsigned int bitValue) {
 // Set "*position" to the beginning bit index of the sequence.
 int maxContinuousOnes(unsigned int bitmap, int * position) {
 	// Add your code here
+}
+
+// It prints the bits in bitmap as 0s and 1s.
+void printBits(unsigned int bitmap) {
+	for (int i = 31; i >= 0; i--) {
+		printf("%d", getBitAt(bitmap, i));
+	}
+	printf("\n");
+	for (int i = 31; i >= 0; i--)
+		printf("%d", i%10);
+	
 }
 
 
--- labs/lab5-src/test_bits.c
+++ labs/lab5-src/test_bits.c
@@ -1,7 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 #include "bits.h"
 
 void test1()
