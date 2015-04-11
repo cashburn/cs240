153244b40045f0bf98d9fc0318f0ad8c2931f687
Colin Ashburn <cashburn@purdue.edu>
2015-04-11 19:14:20 -0400

    2015-04-11

--- labs/lab6-src/test.sh
+++ labs/lab6-src/test.sh
@@ -80,3 +80,464 @@ expect "'^]'."
 send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
 expect "OK\r\n"
 
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE peter spider The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE cashburn test The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+spawn telnet localhost 2030
+expect "'^]'."
+send "SEND-MESSAGE another one The Room Watson, come here, I want you.\r\n"
+expect "OK\r\n"
+
+
